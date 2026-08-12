1404441892ed285e17ae5e19090abed255d93a0b
ZQR0 <yarikrech@gmail.com>
2026-08-12 18:03:59 +0300

    kafka event types added

---
+++ core/src/main/java/com/fallout/core/enums/KafkaEventType.java
@@ -0,0 +1,10 @@
+package com.fallout.core.enums;
+
+public enum KafkaEventType {
+    POINT_OWNER_CHANGED,
+    GARRISON_UPDATED,
+    BATTLE_REPORT,
+    GAME_FINISHED,
+    COMMAND_REJECTED,
+    PLAYER_RESOURCES_UPDATED
+}
