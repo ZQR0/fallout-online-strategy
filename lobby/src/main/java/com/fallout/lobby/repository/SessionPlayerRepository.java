609166db65ae4bd164acf2b21c9e3256f454cd9c
ZQR0 <yarikrech@gmail.com>
2026-09-25 16:57:58 +0300

    application.yaml updated

--- lobby/src/main/java/com/fallout/lobby/repository/SessionPlayerRepository.java
+++ lobby/src/main/java/com/fallout/lobby/repository/SessionPlayerRepository.java
@@ -4,6 +4,8 @@ import com.fallout.lobby.entity.SessionPlayerEntity;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 import java.util.UUID;
 
 /**
@@ -27,5 +29,6 @@ public interface SessionPlayerRepository extends ReactiveCrudRepository<SessionP
      * @param sessionId The unique identifier of the game session.
      * @return A Mono emitting the total count of players.
      */
-    long countBySessionId(UUID sessionId);
+    Mono<Long> countBySessionId(UUID sessionId);
+
 }
