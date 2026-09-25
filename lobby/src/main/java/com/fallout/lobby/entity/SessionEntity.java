4bdc43edd47a19dca02d80621c9d4c766b01c9d6
ZQR0 <yarikrech@gmail.com>
2026-09-25 17:03:45 +0300

    session entity finished

--- lobby/src/main/java/com/fallout/lobby/entity/SessionEntity.java
+++ lobby/src/main/java/com/fallout/lobby/entity/SessionEntity.java
@@ -1,10 +1,27 @@
 package com.fallout.lobby.entity;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
-@Table(name = "game_session")
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("game_session")
 public class SessionEntity {
-
-    
-
+    @Id
+    private UUID id;
+    private String joinCode;
+    private String status; // CANCELED, WAITING, IN_PROGRESS, FINISHED
+    private OffsetDateTime createdAt;
+    private OffsetDateTime startedAt;
+    private OffsetDateTime finishedAt;
+    private Integer maxPlayers;
+    private Integer currentTurn;
+    private Integer winnerPlayerId;
+    private String mapSeed;
 }
